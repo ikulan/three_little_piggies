9e39cef15aa8e091fb3199272fc16998e7d97b2e
ikulan <ikulan12@gmail.com>
2023-12-04 14:23:05 -0800

    Add tooltips

--- src/components/button.ts
+++ src/components/button.ts
@@ -4,13 +4,21 @@ import { Utils } from "../utils/utils";
 export abstract class Button extends EventPublisher<HTMLButtonElement> {
   static EVENTS = ["click"];
   private _img_elem: HTMLImageElement;
+  protected _span_elem: HTMLSpanElement;
 
   constructor(id: string) {
     super(id.toLowerCase(), Button.EVENTS);
 
+    // image
     this._img_elem = new Image();
     this._img_elem.src = Utils.img_path(this.id);
     this._elem.appendChild(this._img_elem);
+
+    // tooltip
+    this._elem.className = "tooltip";
+    this._span_elem = document.createElement("span");
+    this._span_elem.className = "tooltiptext";
+    this._elem.appendChild(this._span_elem);
 
     this.configure();
   }
@@ -22,14 +30,22 @@ export abstract class Button extends EventPublisher<HTMLButtonElement> {
   protected configure(): void {}
 }
 
-export class PreviousButton extends Button {}
+export class PreviousButton extends Button {
+  protected configure(): void {
+    this._span_elem.innerHTML = "Previous Game";
+  }
+}
 export class RestartButton extends Button {
   protected configure(): void {
-    this._elem.title = "Restart this game";
+    this._span_elem.innerHTML = "Restart";
 
     this._elem.addEventListener("click", (e) => {
       this.notify("click");
     });
   }
 }
-export class NextButton extends Button {}
+export class NextButton extends Button {
+  protected configure(): void {
+    this._span_elem.innerHTML = "Next Game";
+  }
+}
